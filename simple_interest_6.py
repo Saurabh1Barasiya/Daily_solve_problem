475417cc20d83e781adb3f6beccdc89edf4c1c0b
Saurabh Barasiya <43789500+Saurabh1Barasiya@users.noreply.github.com>
2020-06-30 21:36:25 +0530

    Add files via upload

---
+++ simple_interest_6.py
@@ -0,0 +1,6 @@
+# Python Program for simple interest
+price = float(input("Please enter the price"))
+rate = float(input("Please enter the rate"))
+time = float(input("Please enter the time (time means years)")) 
+simple_interest = (price * rate * time) / 100      
+print(f"simple_interest is {simple_interest}")     
