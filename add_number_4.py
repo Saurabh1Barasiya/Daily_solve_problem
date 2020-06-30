31e9b3055e26fdc9e3914102287cf1282b7b469c
Saurabh Barasiya <43789500+Saurabh1Barasiya@users.noreply.github.com>
2020-06-30 21:22:58 +0530

    Add files via upload

---
+++ add_number_4.py
@@ -0,0 +1,5 @@
+# Python program to add two numbers
+frist_number = int(input("Enter frist_number "))
+secound_number = int(input("Enter secound_number "))
+result = frist_number + secound_number
+print(f"Addition is {result}")   
