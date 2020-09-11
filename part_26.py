f4d678e20d49905d244324816fc98cd88ce05f14
Saurabh Barasiya <43789500+Saurabh1Barasiya@users.noreply.github.com>
2020-09-11 09:23:02 +0530

    Create part_26.py

---
+++ part_26.py
@@ -0,0 +1,17 @@
+# Print star pattern    
+'''
+*    
+**   
+***
+****
+*****
+
+'''
+
+for i in range(1,6):
+    for j in range(1,6):
+        if j<= i:
+            print("*",end='')  
+        else:     
+            print(" ",end='')    
+    print("")
